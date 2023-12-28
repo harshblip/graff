793a2d1196c4aa1f7440c540f50b26d8a13cc0a5
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2023-12-28 19:46:24 +0530

    route set

---
+++ src/components/Profilepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProfilePage: React.FC = () => {
+    return (
+        <div>
+            profile page
+        </div>
+    );
+};
+
+export default ProfilePage;
