c870ee1b7983ed69fb82a67aeb7549ecb4a9c3d4
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2023-12-29 00:28:52 +0530

    added navbar and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className='main-bg'>
+    <div>
       <Navbar />
       <Routes>
         <Route path = '/' element={<ProfilePage/>} />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from '../images/logo.svg';
+import styled from 'styled-components';
 
 const Navbar: React.FC = () => {
+    const Nav = styled.div`
+        display: flex;
+        justify-content: space-between
+    `
     return (
-        <div>
-            <img src = {logo} alt='genoshi' />
-        </div>
+        <Nav>
+            <img src = {logo} alt='genoshi' className='logo'/>
+            <button className='bt'> Logout </button>
+        </Nav>
         );
 };
 
