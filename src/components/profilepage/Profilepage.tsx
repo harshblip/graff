8dc932889cff311e2c8cf5a9ec51c289643e7735
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2023-12-29 02:14:12 +0530

    profile page responsived

--- src/components/profilepage/Profilepage.tsx
+++ src/components/profilepage/Profilepage.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import './Profilepage.css';
 
 const ProfilePage: React.FC = () => {
     return (
-        <div>
-            profile page
+        <div style={{ display: 'flex' }}>
+            <div className='h-[4rem] w-[20rem] absolute md:relative md:h-[24rem] md:w-[14rem] ml-[4rem] mt-[5rem] border border-white'>
+
+            </div>
+            <div style={{ display: 'flex', flexWrap: 'wrap' }} className='mt-28 md:mt-0'>
+
+                <div className='h-[10rem] w-[20rem] ml-[4rem] mt-[5rem] border border-white'>
+
+                </div>
+                <div className='h-[10rem] w-[20rem]  ml-[4rem] mt-[5rem] border border-white'>
+
+                </div>
+                <div className='h-[8rem] w-[100%] mr-24 ml-[4rem] mt-[5rem] border border-white'>
+
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,26 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '850px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
-        "inter" : ['Inter', 'sans-serif'],
-        "poppins" : ['Poppins', 'sans-serif'],
+        "inter": ['Inter', 'sans-serif'],
+        "poppins": ['Poppins', 'sans-serif'],
       }
     },
   },
